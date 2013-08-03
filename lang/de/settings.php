abf1ebcdfc89204ec78ec69704f0d815df9a82a3
Dominik Eckelmann <deckelmann@gmail.com>
2013-08-03 23:15:10 +0200

    translation update

---
+++ lang/de/settings.php
@@ -0,0 +1,18 @@
+<?php
+
+/**
+ * @license    GPL 2 (http://www.gnu.org/licenses/gpl.html)
+ * 
+ * @author Dominik Eckelmann <deckelmann@gmail.com>
+ */
+$lang['smtp_host']             = 'Dein ausgehender SMTP Server';
+$lang['smtp_port']             = 'Der Port deines SMTP Servers (Standard 25 oder 465 für SSL)';
+$lang['smtp_ssl']              = 'Welche Verschlüsslung wird bei der Kommunikation mit deinem SMTP Server verwendet.';
+$lang['smtp_ssl_o_8']          = 'Keine';
+$lang['smtp_ssl_o_4']          = 'SSL';
+$lang['smtp_ssl_o_2']          = 'TLS';
+$lang['auth_user']             = 'Trage hier den Benutzernamen ein, falls eine Anmeldung am STMP Server erforderlich ist.';
+$lang['auth_pass']             = 'Passwort für den Benutzer darüber.';
+$lang['pop3_host']             = 'Wenn dein Server POP-vor-SMPT Authentifizierung verwendet, gib deine POP3 Anmeldedaten darüber an und hier deinen POP3 Server. Für SMTP Authentifizierung lasse das Feld leer. ';
+$lang['localdomain']           = 'Füge hier den namen, welcher während des HELO Aufrufes von SMTP verwendet werden soll. Dies sollte der FQDN deines DokuWiki Webservers sein. Wenn du es Leer lässt wird DokuWiki versuchen es selbst zu ermitteln.';
+$lang['debug']                 = 'Kompletten Fehlerbericht ausgeben wenn das Versenden fehlschlägt. Deaktiviere es wenn das Versenden Funktioniert.';
