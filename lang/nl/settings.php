6c55def4985b50225c25c39add895047a6ba09cf
Gerrit Uitslag <Klapinklapin@gmail.com>
2014-01-06 22:30:27 +0100

    translation update

---
+++ lang/nl/settings.php
@@ -0,0 +1,18 @@
+<?php
+
+/**
+ * @license    GPL 2 (http://www.gnu.org/licenses/gpl.html)
+ * 
+ * @author Gerrit Uitslag <Klapinklapin@gmail.com>
+ */
+$lang['smtp_host']             = 'Je uitgaande SMTP server.';
+$lang['smtp_port']             = 'De poort waarop je SMTP server luistert. Vaak 25. 465 voor SSL.';
+$lang['smtp_ssl']              = 'Welke soort encryptie is gebruikt om te communiceren met je SMTP server?';
+$lang['smtp_ssl_o_8']          = 'geen';
+$lang['smtp_ssl_o_4']          = 'SSL';
+$lang['smtp_ssl_o_2']          = 'TLS';
+$lang['auth_user']             = 'Als authenticatie nodig is, moet je hier je gebruikersnaam geven.';
+$lang['auth_pass']             = 'Wachtwoord van bovenstaande gebruiker.';
+$lang['pop3_host']             = 'Als je server POP-voor-SMTP gebruikt voor authenticatie, geef je jouw POP3 inloggegevens hierboven en vul je hier je POP3 server in. Voor standaard SMTP authenticatie dit veld leeglaten.';
+$lang['localdomain']           = 'De naam die gebruikt wordt tijdens de HELO fase van SMTP. Moet de FQDN van de webserver zijn waarop DokuWiki draait. Leeglaten voor zelfdetectie.';
+$lang['debug']                 = 'Een complete foutlog weergeven als het versturen mislukt? Schakel deze optie uit als alles werkt!';
