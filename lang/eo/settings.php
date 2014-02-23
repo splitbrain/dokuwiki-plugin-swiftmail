99b5dc28c7977f039e656338b8db273ef77c976d
Robert Bogenschneider <bogi@uea.org>
2014-02-23 08:50:07 +0100

    translation update

---
+++ lang/eo/settings.php
@@ -0,0 +1,18 @@
+<?php
+
+/**
+ * @license    GPL 2 (http://www.gnu.org/licenses/gpl.html)
+ * 
+ * @author Robert Bogenschneider <bogi@uea.org>
+ */
+$lang['smtp_host']             = 'Via SMTP-servilo por eliranta poŝto.';
+$lang['smtp_port']             = 'La SMTP-pordego de via servilo. Kutime 25, 465 por SSL.';
+$lang['smtp_ssl']              = 'Kia ĉifrado estas uzata dum komunikado kun via SMTP-servilo?';
+$lang['smtp_ssl_o_8']          = 'neniu';
+$lang['smtp_ssl_o_4']          = 'SSL';
+$lang['smtp_ssl_o_2']          = 'TLS';
+$lang['auth_user']             = 'Se aŭtentikado necesas, indiku vian nomon tie.';
+$lang['auth_pass']             = 'Pasvorto por la supra uzanto.';
+$lang['pop3_host']             = 'Se via servilo uzas POP-antaŭ-SMTP, indiku viajn POP3-datumojn supre kaj indiku vian POP3-servilon ĉi tie. Por kutima SMTP lasu la kampon malplena.';
+$lang['localdomain']           = 'Nomo dum la HELO-fazo de SMTP. Devus esti la FQDN de la retservilo, kiun uzas DokuWiki. Lasu malplena por aŭtomata detekto.';
+$lang['debug']                 = 'Ĉu presi plenan erar-logon, kiam sendado malsukcesas? Malŝaltu, kiam ĉio funkcias!';
