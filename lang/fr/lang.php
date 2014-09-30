db44fbcf8dceb67b7987487abb18614dcc45c4a3
Schplurtz le Déboulonné <Schplurtz@laposte.net>
2014-09-30 08:40:13 +0200

    translation update

--- lang/fr/lang.php
+++ lang/fr/lang.php
@@ -4,6 +4,7 @@
  * @license    GPL 2 (http://www.gnu.org/licenses/gpl.html)
  * 
  * @author Schplurtz le Dévoulonné <Schplurtz@laposte.net>
+ * @author Schplurtz le Déboulonné <Schplurtz@laposte.net>
  */
-$lang['menu']                  = 'Vérifier la cofiguration de SwiftMail';
+$lang['menu']                  = 'Vérifier la configuration de SwiftMail';
 $lang['nofrom']                = 'Vous n\'avez pas configuré l\'option \'De\'. L\'envoi de courriel va probablement échouer.';
