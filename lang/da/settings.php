cf12a3405cd36a1f6872de99ee486d2a91bc75a8
Søren Birk <soer9648@eucl.dk>
2014-12-11 12:00:06 +0100

    translation update

---
+++ lang/da/settings.php
@@ -0,0 +1,18 @@
+<?php
+
+/**
+ * @license    GPL 2 (http://www.gnu.org/licenses/gpl.html)
+ * 
+ * @author Søren Birk <soer9648@eucl.dk>
+ */
+$lang['smtp_host']             = 'Din udgående SMTP-server.';
+$lang['smtp_port']             = 'Porten, hvor din SMTP-server lytter. Som regel 25. 465 for SSL.';
+$lang['smtp_ssl']              = 'Hvilken kryptering benyttes under kommunikation med din SMTP-Server?';
+$lang['smtp_ssl_o_8']          = 'ingen';
+$lang['smtp_ssl_o_4']          = 'SSL';
+$lang['smtp_ssl_o_2']          = 'TLS';
+$lang['auth_user']             = 'Hvis autentificering er nødvendigt, skriv dit brugernavn her.';
+$lang['auth_pass']             = 'Adgangskode for brugeren herover.';
+$lang['pop3_host']             = 'Hvis din server benytter POP-før-SMTP til autentificering, skal du angive dine POP3-legitimistionoplysninger herover, og indtaste din POP3-server her. For almindelig SMTP-aut efterlades dette felt tomt.';
+$lang['localdomain']           = 'Navnet der skal benyttes under HELP-fasen for SMTP. Bør være FQDN for webserveren som DokuWiki kører på. Efterlad feltet tomt for autogenkendelse.';
+$lang['debug']                 = 'Udskriv en fuld fejllog når mails sendes? Deaktivér dette, når alt virker!';
